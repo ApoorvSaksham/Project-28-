157af9c2db124483ac9d26698fdb9736ce67bc22
ApoorvSaksham <79132789+ApoorvSaksham@users.noreply.github.com>
2021-06-21 20:12:58 +0530

    Add files via upload

---
+++ js/Launcher.js
@@ -0,0 +1,33 @@
+class Launcher{
+    constructor(body1, point2){
+   
+        var optionsC = {
+
+            bodyA: body1,
+            pointB: point2,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = point2;
+        this.chain = Constraint.create(optionsC);
+        World.add(world,this.chain);
+
+    }
+    display(){
+    if(this.chain.bodyA){
+
+    
+var pointA = this.chain.bodyA.position;
+var pointB = this.pointB;
+strokeWeight(3);
+line(pointA.x, pointA.y, pointB.x, pointB.y);
+
+    }
+    }
+fly(){
+    this.chain.bodyA = null;
+}
+attach(body1){
+    this.chain.bodyA = body1;
+}
+}
